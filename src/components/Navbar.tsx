d5ae0722111ba919e3749460e78e8d64851179c3
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-04-08 18:29:43 +0530

    logout added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,41 @@
-'use client';
+'use client'
+import axios from 'axios';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import toast, { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
-  const link = usePathname();
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      const response = await axios.get('/api/users/logout');
+      console.log(response);
 
+      toast.success('Logged out successfully');
+      router.push('/auth/login')
+    } catch (error) {}
+  };
   return (
     <div>
+      <Toaster />
       <div className='bg-transparent px-24 backdrop-blur-md bg-opacity-20 flex justify-between py-12 text-lg'>
-        <h1 className='flex justify-between items-center'>
-          Global Center
-        </h1>
+        <h1 className='flex justify-between items-center'>Global Center</h1>
         <div className='space-x-8 flex justify-between items-center'>
           <Link href={'/'} className=''>
             Home
           </Link>
-          <Link href={'/missions'} className='py-2 px-6 rounded-lg bg-teal-900 border border-teal-400 text-white'>
+          <Link
+            href={'/missions'}
+            className='py-2 px-6 rounded-lg bg-teal-900 border border-teal-400 text-white'
+          >
             Missions
           </Link>
-          <Link href={'/'} className='py-2 px-6 rounded-lg bg-red-900 border border-red-400 text-white'>
-            DeadZone
-          </Link>
+          <button
+            onClick={logout}
+            className='py-2 px-6 rounded-lg bg-red-900 border border-red-400 text-white'
+          >
+            Logout
+          </button>
         </div>
       </div>
     </div>
